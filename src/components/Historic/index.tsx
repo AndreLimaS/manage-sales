a6aa03b192657ed7c25c607179785ff3104fb1ea
AndreLimaS <a.s.l0@hotmail.com>
2023-04-05 15:05:48 -0300

    feat: create struct component Historic

---
+++ src/components/Historic/index.tsx
@@ -0,0 +1,32 @@
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Historic() {
+  return (
+    <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighlight variant="outcome">R$ -12.000,00</PriceHighlight>
+            </td>
+            <td>Alimentacao</td>
+            <td>13/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+    </TransactionsContainer>
+  );
+}
